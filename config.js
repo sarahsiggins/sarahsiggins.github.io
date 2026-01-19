fac747f258c8dd26c52b782ab6b2e0490e3796b7
Sarah Christen <sarchristen@gmail.com>
2026-01-19 18:05:36 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,60 @@
+window.PorfolioConfig = {
+  name: 'Sarah Siggins',
+  jobTitle: 'Data Science Student',
+  jobDescription: 'write this later',
+  skills: [
+    {
+      name: 'Python',
+      description: 'Reprehenderit mollit id ad veniam.'
+    },
+    {
+      name: 'R',
+      description: 'Fugiat veniam esse consequat quis cillum elit consequat Lorem pariatur ipsum veniam irure incididunt non.'
+    }
+  ],
+  works: [
+    {
+      name: 'Vue Stripe Checkout',
+      link: 'https://jofftiquez.github.io/vue-stripe-checkout',
+      description: 'Mollit dolor tempor adipisicing non pariatur in tempor cillum do. In officia ullamco laborum ullamco voluptate. Laboris sunt quis enim Lorem laboris aliquip duis non magna excepteur voluptate. Pariatur velit aliquip officia nulla anim reprehenderit deserunt. Sint nostrud ipsum do aliquip.'
+    },
+    {
+      name: 'ToDo App',
+      link: 'https://theuselessweb.com/',
+      description: 'Esse magna anim incididunt enim sint aute consectetur ipsum id nostrud eu do. Pariatur et anim enim aliquip anim. Laboris labore incididunt labore enim nisi elit excepteur deserunt irure exercitation exercitation. Lorem sit ad laborum sit velit veniam sit nisi nostrud occaecat duis nulla duis.'
+    },
+    {
+      name: 'A Mobile Game',
+      link: 'https://theuselessweb.com/',
+      description: 'Incididunt non cupidatat deserunt ut commodo qui non do Lorem. Qui aute minim enim sit magna est consectetur incididunt. Ex est cupidatat tempor pariatur deserunt labore tempor minim laboris magna ipsum minim ea ea. Consectetur do anim laboris nulla nisi veniam sint qui.'
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Running',
+      description: 'Occaecat ipsum dolor ea ad ut irure do proident excepteur consectetur duis eu culpa velit.'
+    },
+    {
+      name: 'Baking',
+      description: 'Laboris occaecat adipisicing culpa duis aute ea excepteur quis eiusmod mollit est pariatur.'
+    },
+    {
+      name: 'Puzzles',
+      description: 'Laborum pariatur sint labore officia enim occaecat id.'
+    }
+  ],
+  contacts: {
+    email: 'sarahhsiggins@gmail.com',
+    mobileNo: '703 424 0840',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/sarahsiggins'
+      },
+      {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/sarahhsiggins/'
+      }
+    ]
+  }
+};
